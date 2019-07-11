0703a153e11e4574d98f1a2379006efc918da0e1
Teodora <bla@bla.com>
2019-07-11 17:50:31 +0200

    add DbContext

---
+++ src/TimeTrackerLukaN/Data/TimeTrackerDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeTrackerLukaN.Domain;
+
+namespace TimeTrackerLukaN.Data
+{
+    public class TimeTrackerDbContext : DbContext
+    {
+
+        public TimeTrackerDbContext(DbContextOptions<TimeTrackerDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TimeEntry> TimeEntries { get; set; }
+    }
+}
