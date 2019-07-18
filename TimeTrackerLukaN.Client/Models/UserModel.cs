886cd5ff02232fe81a1491a5db81d66c5bbe379b
luka <lukananic2@gmail.com>
2019-07-18 17:26:21 +0200

    add tokenAuthStateProvider class and auth infrastructure

---
+++ TimeTrackerLukaN.Client/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackerLukaN.Client.Models
+{
+    public class UserModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal HourRate { get; set; }
+    }
+}
---
+++ TimeTrackerLukaN.Client/Security/SecurityExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTrackerLukaN.Client.Security
+{
+    public static class SecurityExtensions
+    {
+        public static void AddTokenAuthenticationStateProvider(this IServiceCollection services)
+        {
+            services.AddScoped<TokenAuthenticationStateProvider>();
+            services.AddScoped<AuthenticationStateProvider>(provider =>
+                    provider.GetRequiredService<TokenAuthenticationStateProvider>());
+        }
+    }
+}
